7bc0506be6ca1b7aa165116869eb813a5a8bd7b0
Melani Sosa <sosa92979@gmail.com>
2025-04-09 06:12:25 +0200

    Prepare mocked data for presentation in buscarTrabajador and calcularPromedio modules

--- src/modules/calcularPromedio/calcularPromedio.module.ts
+++ src/modules/calcularPromedio/calcularPromedio.module.ts
@@ -3,15 +3,17 @@ import { CalcularPromedioController } from "./calcularPromedio.controller";
 import { CalcularPromedioService } from "./calcularPromedio.service";
 import { HttpModule } from "@nestjs/axios";
 import { ConfigModule } from "@nestjs/config";
-
+import { ExportUtilities } from "../export/export.utility";
+import { ExportModule } from "../export/export.module";
 
 
 @Module({
   imports: [
     HttpModule.register({}), // Configuración básica
     ConfigModule,
+    ExportModule,
   ],
   controllers: [CalcularPromedioController],
-  providers: [CalcularPromedioService],
+  providers: [CalcularPromedioService, ExportUtilities],
 })
 export class CalcularPromedioModule{}
